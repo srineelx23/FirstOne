048156e01857fa83d79baa79dc0027f1fae287a4
Srineel Reddy <srineelx23@gmail.com>
2025-05-08 20:57:38 +0530

    change 1

--- src/components/Assignment-10/Home/Home.js
+++ src/components/Assignment-10/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div className='p-5'>
-      <h1 className='bg-white p-3 rounded-3 mx-auto w-50 mt-5'>Welcome to User DashBoard Implementation and login with support of local API</h1>
+      <h1 className='bg-white p-3 rounded-3 mx-auto w-50 mt-5'>Welcome to User-DashBoard Implementation and login with support of local API</h1>
     </div>
   )
 }
